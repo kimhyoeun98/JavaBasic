51585c215cf253dd23c464b44f8c9ed5166baa51
kimhyoeun98 <wldusiu@gmail.com>
2025-04-10 21:36:49 +0900

    Delete hw250408 directory

--- hw250408/src/Cheapter04_challege01/PrinterTest.java
+++
@@ -1,9 +0,0 @@
-package Cheapter04_challege01;
-
-public class PrinterTest {
-    public static void main(String[] args) {
-        Printer p = new Printer(100);
-        p.print(70);
-        System.out.println(p.getNumOfPapers());
-    }
-}
